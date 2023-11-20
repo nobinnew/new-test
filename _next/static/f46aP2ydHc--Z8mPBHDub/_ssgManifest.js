3ac86f8bb10a041c722326169e1a5d25d0f6c5cf
Nobin <nobin.com@gmail.com>
2023-11-20 15:22:57 +0600

    Initial Commit

---
+++ _next/static/f46aP2ydHc--Z8mPBHDub/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog-2","\u002Fblog\u002F[id]","\u002Fblog\u002Fpage\u002F[page]","\u002Fhome-2","\u002Fprojects","\u002Fprojects-2","\u002Fprojects-3","\u002Fprojects-4","\u002Fprojects-5","\u002Fprojects\u002F[id]","\u002Fservices","\u002Fservices\u002F[id]","\u002Fstory"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
